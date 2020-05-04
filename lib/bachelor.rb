ec01c5e4124289d9105fe2479f55096b40be74f2
Jo Fischer <josephinefischer9@gmail.com>
2020-05-04 12:01:57 -0400

    Second commit

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -11,7 +11,13 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |key, contestants|
+    contestants.each do |contestant_hash|
+      if contestant_hash["occupation"] == occupation
+        return contestant_hash["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
@@ -23,5 +29,4 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  # code here
 end
