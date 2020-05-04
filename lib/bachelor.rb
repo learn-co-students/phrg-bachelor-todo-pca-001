52d42bf723245adfa4702e252de058d311211ae9
Liam Tinney <ltinney@powerhrg.com>
2020-05-04 10:51:46 -0400

    Bachelor Hash

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,18 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+    data[season].each do |contestant|
+      if contestant ["status"] == "Winner"
+        return contestant["name"].split.first
+      end
+    end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+    contestant_name = " "
+  data.each do |season, people|
+      people.each do |contestant|
+      end
+    end
+  return contestant_name
 end
 
 def count_contestants_by_hometown(data, hometown)
